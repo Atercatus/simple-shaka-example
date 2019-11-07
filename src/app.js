916e6ea99e895c941c79666ca42bef11a0737d09
Atercatus <myleelee2053@naver.com>
2019-11-07 19:10:16 +0900

    refactor

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const request = require("request-promise");
 
 const STATUS = require("./vendor/status");
 const { makeOption } = require("./vendor/helper");
+const S3 = require("./vendor/bucket");
 
 const app = express();
 const PORT = process.env.PORT || 4040;
@@ -15,35 +16,64 @@ app.use("/js", express.static("public/js"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/test", async (req, res) => {
-  const { jobId, status } = req.body;
-  console.log(req.body);
-  if (status !== STATUS.success) {
-    return;
-  }
+function isSuccess(status) {
+  return status === STATUS.success;
+}
 
-  const now = Date.now().toString();
-
-  const option = makeOption({
+function getVODTranscoderAPIOption({ method, timestamp, path }) {
+  return makeOption({
     host: process.env.VOD_TRANSACTION_HOST,
-    method: "GET",
-    path: `/api/v2/jobs/${jobId}`,
-    timestamp: now,
+    method,
+    path: `/api/v2/jobs/${path}`,
+    timestamp,
     apiKey: process.env.API_KEY_PRI,
     accessKey: process.env.ACCESS_KEY_ID,
     secretKey: process.env.SECRET_KEY
   });
+}
+
+async function getJobOutput(option) {
+  const result = await request(option);
+  const jobs = JSON.parse(result).jobs;
+  return jobs[0].output;
+}
+
+app.post("/test", async (req, res) => {
+  const { jobId, status } = req.body;
+  console.log(req.body);
+
+  const now = Date.now().toString();
+
+  if (!isSuccess(status)) return;
+
+  const option = getVODTranscoderAPIOption({
+    timestamp: now,
+    method: "GET",
+    path: jobId
+  });
+
+  // makeOption({
+  //   host: process.env.VOD_TRANSACTION_HOST,
+  //   method: "GET",
+  //   path: `/api/v2/jobs/${jobId}`,
+  //   timestamp: now,
+  //   apiKey: process.env.API_KEY_PRI,
+  //   accessKey: process.env.ACCESS_KEY_ID,
+  //   secretKey: process.env.SECRET_KEY
+  // });
 
   const _option = {
     url: `https://${process.env.VOD_TRANSACTION_HOST}${option.path}`,
     headers: option.headers
   };
-  console.log(_option);
 
-  const result = await request(_option);
-  const jobs = JSON.parse(result).jobs;
-  const output = jobs[0].output;
-  console.log("output", output);
+  // const result = await request(_option);
+  // const jobs = JSON.parse(result).jobs;
+  // const output = jobs[0].output;
+  // console.log("output", output);
+
+  const output = getJobOutput(_option);
+  console.log(output);
 
   const bucketName = process.env.BUCKET_NAME;
 });
